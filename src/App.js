0464db02978501a68ff4a6654aa217e1ccde9d05
Jacob Pyke <pyke.jacob1@gmail.com>
2019-03-20 22:13:03 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dbData: []
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    axios
+      .get("http://localhost:5000/api/v1/todos")
+      .then(response => {
+        for (let i = 0; i < response.data.todos.length; i++) {
+          const element = response.data.todos[i];
+          this.setState({
+            dbData: element,
+          })
+        }
+      });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div className="button__container">
+            <button className="button" onClick={this.handleClick}>
+              Click Me
+            </button>
+            <p>{this.state.dbData.name}</p>
+          </div>
         </header>
       </div>
     );
---
+++ src/MyComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class test extends Component {
+    render(){
+        return (
+            <p>Test</p>
+        );
+    }
+}
+
+export default test;
